126bd87f6ecd98c42a285e4f7e71bfe2cec13b42
Jorge Brunetto <jorgebrunetto@hotmail.com>
2022-12-08 12:21:59 -0300

    feat: finish page

---
+++ _layouts/src/assets/js/app.js
@@ -0,0 +1,23 @@
+tippy('#bugs', {
+    content: 'Bugs reports',
+});
+
+tippy('#code', {
+    content: 'Code',
+});
+
+tippy('#maintenance', {
+    content: 'Maintenance',
+});
+
+tippy('#mentoring', {
+    content: 'Mentoring',
+});
+
+tippy('#bugs', {
+    content: 'Bugs reports',
+});
+
+tippy('#design', {
+    content: 'Design',
+});
